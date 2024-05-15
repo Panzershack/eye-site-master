cc155cb81d06d2e02223741c0ea47e1a878b7a31
Panzershack <pasannim@yahoo.com>
2024-05-15 14:47:52 +0530

    functionalities all ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,9 +18,10 @@ import {Routes, Route} from 'react-router-dom'
 
 import OurServicesComponent from './pages/OurServicesComponent';
 import AdminDashBoardComponent from './pages/AdminDashBoard';
+import ProductPage from './pages/ProductPage';
 import RegisterAndLogin from './authentication/RegisterAndLogin';
 import ResetPass from './authentication/ForgotPassword';
-
+import HomeComponent from './pages/HomeComponent';
 
 
 
@@ -40,15 +41,13 @@ const App = () => {
       <Route path='/inquiries/edit/:id' element={<EditInquiry/>}/>
       <Route path='/inquiries/delete/:id' element={<DeleteInquiry/>}/> */}
 
+        <Route path="/" element={<HomeComponent />} />
         <Route path="/admin" element={<AdminDashBoardComponent />} />
-        <Route path="/" element={<OurServicesComponent />}></Route>
+        <Route path="/ourServices" element={<OurServicesComponent />}></Route>
+        <Route path="/products" element={<ProductPage />}></Route>
 
-        <Route path="/reglog" element={<RegisterAndLogin/>}></Route>
-        <Route path="/reset" element={<ResetPass/>}></Route>
-
-
-
-       
+        <Route path="/reglog" element={<RegisterAndLogin />}></Route>
+        <Route path="/reset" element={<ResetPass />}></Route>
       </Routes>
     </>
   );
